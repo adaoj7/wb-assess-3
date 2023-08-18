359b311837094d9174e1658de9bba170647d3e58
Adam Johnson <adaojohnson902@gmail.com>
2023-08-18 14:47:31 -0600

    random fossil implemented

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -2,8 +2,15 @@ console.log('Hello World');
 import axios from "axios";
 
 async function randomFossil() {
-
-   const response = axios.get()
+   const url =  '/random-fossil.json'
+   const response = await axios.get(url)
+   console.log(response)
+   const img = response.data.img
+   const name = response.data.name
+   console.log(name)
+   console.log(img)
+   document.querySelector('#random-fossil-image').innerHTML = `<img src=${img}/>`
+   document.querySelector('#random-fossil-name').innerText = name
 }
 
 const button = document.querySelector('#get-random-fossil')
